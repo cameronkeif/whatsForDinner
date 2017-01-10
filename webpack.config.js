83deac632f4d5c61ed113eb1490d1ba66d42c429
Cam <cam@localhost.localdomain>
2017-01-09 19:53:07 -0600

    Webpack, NPM, and linter settings
    Webpack, NPM, and linter settings

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+/* eslint-disable */
+
+var webpack = require('webpack');
+var path = require('path');
+
+var BUILD_DIR = path.resolve(__dirname, 'client/public/');
+var APP_DIR = path.resolve(__dirname, 'client/app/');
+
+var config = {
+  entry: APP_DIR + '/index.jsx',
+  output: {
+    path: BUILD_DIR,
+    filename: 'bundle.js'
+  },
+  module : {
+    loaders : [
+      {
+        test : /\.jsx?/,
+        include : APP_DIR,
+        loader : 'babel'
+      }
+    ]
+  },
+  resolve: {
+    extensions: ["", ".js", ".jsx"]
+  },
+};
+
+module.exports = config;
