634730a363e6973cf7eeb037d1c7f82abd13a3ac
Cam <cameron.keif@gmail.com>
2017-01-09 22:48:56 -0600

    Add LESS processing and simple stylesheet
    Add LESS processing and simple stylesheet

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@
 
 var webpack = require('webpack');
 var path = require('path');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 var BUILD_DIR = path.resolve(__dirname, 'client/public/');
 var APP_DIR = path.resolve(__dirname, 'client/app/');
@@ -12,18 +13,27 @@ var config = {
     path: BUILD_DIR,
     filename: 'bundle.js'
   },
+  devtool: 'source-map',
   module : {
     loaders : [
       {
         test : /\.jsx?/,
         include : APP_DIR,
         loader : 'babel'
+      },
+      {
+          test: /\.less$/,
+          loader: "style!css!less"
       }
     ]
   },
   resolve: {
     extensions: ["", ".js", ".jsx"]
   },
+  plugins: [
+        // extract inline css into separate 'styles.css'
+        new ExtractTextPlugin('styles.css')
+    ]
 };
 
 module.exports = config;
