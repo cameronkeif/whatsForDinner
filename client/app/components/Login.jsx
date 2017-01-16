dd326bd8432805db9db34d54d1cc574e1eb28098
Cam <cameron.keif@gmail.com>
2017-01-16 11:00:13 -0600

    Remember me checkbox and login button
    Remember me checkbox and login button

--- client/app/components/Login.jsx
+++ client/app/components/Login.jsx
@@ -3,9 +3,22 @@ import React from 'react';
 function Login() {
     return (
       <div className="login independant-pane">
-        <div>Log in</div>
-        <div><input placeholder={'Username'} /></div>
-        <div><input placeholder={'Password'} type={'password'} /></div>
+        <div>
+          <h2>Log in</h2>
+        </div>
+        <div>
+          <input id="username" placeholder={'Username'} />
+        </div>
+        <div>
+          <input id="password" placeholder={'Password'} type={'password'} />
+        </div>
+        <div>
+          <input id="remember-me" type="checkbox" />
+          <label htmlFor="remember-me">Remember me</label>
+        </div>
+        <div>
+          <button type="button" className="positive-button" id="login-button">Log in</button>
+        </div>
       </div>
     );
 }
