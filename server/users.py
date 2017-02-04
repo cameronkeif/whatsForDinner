cdcd761178db8a6245b74a489d4f9dffeef92eb9
Cam <cameron.keif@gmail.com>
2017-02-03 22:55:38 -0600

    Allow user registration at /api/users/register
    Allow user registration at /api/users/register

--- server/users.py
+++ server/users.py
@@ -3,19 +3,46 @@
 from flask import Blueprint
 from flask import request
 from flask import jsonify
+from flask import json
+
+import bcrypt
 
 from db import DB
 from models.User import User
 
 USERS = Blueprint('users', __name__)
 
-@USERS.route('/api/users')
+@USERS.route('/api/users/authenticate')
 def get():
     """Handles a read operation on users table"""
     status = 'failed'
     email = request.args.get('email')
     password = request.args.get('password')
     user = User.query.filter_by(email=email).first()
-    if (user):
-        pass # check hashed password, set status = success if match
+    if user and bcrypt.hashpw(password.encode('utf-8'), user.password.encode('utf-8')) == user.password:
+        status = 'success' # User exists and the password matches
     return jsonify({'result': status})
+
+@USERS.route('/api/users/register', methods = ['POST'])
+def register_user():
+    """Handles a read operation on users table"""
+    email = request.args.get('email')
+    password = request.args.get('password')
+
+    if not (email and password):
+        status = 'failed' # Email or password was not provided
+        reason = 'Email or password was not provided.'
+    elif User.query.filter_by(email = email).first() is not None:
+        status = 'failed' # user already exists
+        reason = '{0} already exists.'.format(email)
+    else:
+        hashedPassword = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+        user = User(email, hashedPassword)
+        DB.session.add(user)
+        DB.session.commit()
+        status = "success"
+
+    returnJsonDict = {'result': status}
+    if (status == 'failed'):
+        returnJsonDict['reason'] = reason
+    return jsonify(returnJsonDict)
