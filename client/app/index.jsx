1a3e981c60c18ef3f14a0fefe088abcd3e1008ee
Cam <cameron.keif@gmail.com>
2017-02-04 21:58:04 -0600

    Add missing semi-colon
    Add missing semi-colon
    
    Missed this lint warning xD

--- client/app/index.jsx
+++ client/app/index.jsx
@@ -3,7 +3,7 @@ import cookie from 'react-cookie';
 import { render } from 'react-dom';
 
 import Login from './components/Login';
-import Overview from './components/Overview'
+import Overview from './components/Overview';
 import css from '../assets/stylesheets/styles.less';
 
 if (cookie.load('whatsForDinnerSessionCookie')) {
