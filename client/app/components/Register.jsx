f683702661631dc0b877b0fc99f5a89c441971e2
Cam <cameron.keif@gmail.com>
2017-02-18 11:20:20 -0600

    Add registration component

--- client/app/components/Register.jsx
+++ client/app/components/Register.jsx
@@ -2,38 +2,33 @@ import React from 'react';
 import request from 'then-request';
 import { render } from 'react-dom';
 
-import Overview from './Overview';
-import Register from './Register';
+import Login from './Login';
 
-const loginSubmit = () => {
+const registerSubmit = () => {
     const invalidLoginNotification = document.getElementById('invalid-login-notification');
     invalidLoginNotification.style.display = 'none';
 
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
     const options = { qs: { email, password } };
-    request('GET', '../api/users/authenticate', options).done((res) => {
+    request('POST', '../api/users/register', options).done((res) => {
         const response = JSON.parse(res.getBody());
         if (response.result === 'success') {
-            render(<Overview />, document.getElementById('app'));
+            alert('Registration successful!'); // Kludgey notification, will update later.
+            render(<Login />, document.getElementById('app'));
         } else {
+            invalidLoginNotification.innerHTML = response.reason;
             invalidLoginNotification.style.display = 'block';
         }
     });
-
-    return false;
 };
 
-const showRegistration = () => {
-    render(<Register />, document.getElementById('app'));
-};
-
-function Login() {
+function Register() {
     return (
       <div className="login independant-pane">
         <form>
           <div>
-            <h2>Log in</h2>
+            <h2>Register</h2>
           </div>
           <div>
             <input id="email" placeholder={'Email'} />
@@ -42,15 +37,20 @@ function Login() {
             <input id="password" placeholder={'Password'} type={'password'} />
           </div>
           <div>
-            <input type="button" className="positive-button" id="login-button" onClick={loginSubmit} value="Submit" />
-            <div id={'invalid-login-notification'}>Login failed.</div>
+            <input id="passwordConfirm" placeholder={'Confirm Password'} type={'password'} />
           </div>
           <div>
-            <span onClick={showRegistration} id="register-link">Register</span>
+            <input
+              type="button"
+              className="positive-button"
+              id="login-button"
+              onClick={registerSubmit} value="Register"
+            />
+            <div id={'invalid-login-notification'} />
           </div>
         </form>
       </div>
     );
 }
 
-export default Login;
+export default Register;
